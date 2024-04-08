abd01dd3c33197d349ac5921072f6e01dd6b688a
slavaPanfilov29 <s_panfil290502@mail.ru>
2024-04-08 13:20:39 +0300

    delete variable a

--- main.py
+++ main.py
@@ -42,7 +42,6 @@ def makePreviewText(s):
     filePreviews.clear()    
 
     rows = s.split('\n')
-    b = 0
     
     for i in reversed(range(len(rows))):
         if (rows[i].find(startDirectory) == -1):
